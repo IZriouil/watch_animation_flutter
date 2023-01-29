88454c1683b6b7833044778eb2167b5b804d7a07
ZRIOUIL Issam <izriouil@gmail.com>
2023-01-29 17:59:04 +0100

    model evol

--- lib/watch.model.dart
+++ lib/watch.model.dart
@@ -3,19 +3,83 @@ class Watch {
   final String provider;
   final int price;
   final String assetImage;
+  final String movement;
+  final String casetype;
 
-  Watch({required this.name, required this.provider, required this.price, required this.assetImage});
+  Watch({required this.name, required this.provider, required this.price, required this.assetImage, required this.movement, required this.casetype});
 }
 
 List<Watch> mockWatches = [
-  Watch(name: "Trilobe T1", provider: "Trilobe", price: 1000, assetImage: "assets/images/trilobe_t1.png"),
-  Watch(name: "Longines L1", provider: "Longines", price: 1500, assetImage: "assets/images/longines_l1.png"),
-  Watch(name: "Luminor LM1", provider: "Luminor", price: 1700, assetImage: "assets/images/luminor_lm1.png"),
-  Watch(name: "Zenith Z1", provider: "Zenith", price: 800, assetImage: "assets/images/zenith_z1.png"),
-  Watch(name: "Trilobe T2", provider: "Trilobe", price: 1700, assetImage: "assets/images/trilobe_t2.png"),
-  Watch(name: "Longines L2", provider: "Longines", price: 1300, assetImage: "assets/images/longines_l2.png"),
-  Watch(name: "Luminor LM2", provider: "Luminor", price: 1900, assetImage: "assets/images/luminor_lm2.png"),
-  Watch(name: "Zenith Z2", provider: "Zenith", price: 1000, assetImage: "assets/images/zenith_z2.png"),
-  Watch(name: "Trilobe T3", provider: "Trilobe", price: 1500, assetImage: "assets/images/trilobe_t3.png"),
-  Watch(name: "Longines L3", provider: "Longines", price: 1700, assetImage: "assets/images/longines_l3.png"),
+  Watch(
+    name: "Mystique",
+    provider: "Trilobe",
+    price: 1000,
+    assetImage: "assets/images/trilobe_mystique.png",
+    movement: "Japanese Movement Quartz",
+    case: "40mm Polished Stainless Steel"
+  ),
+  Watch(
+    name: "Elegance",
+    provider: "Longines",
+    price: 1500,
+    assetImage: "assets/images/longines_elegance.png",
+    movement: "Swiss Movement Mechanical",
+    case: "38mm Rose Gold Plated"
+  ),
+  Watch(
+    name: "Radiance",
+    provider: "Luminor",
+    price: 1700,
+    assetImage: "assets/images/luminor_radiance.png",
+    movement: "Swiss Movement Automatic",
+    case: "42mm Black PVD Coated"
+  ),
+  Watch(
+    name: "Voyager",
+    provider: "Zenith",
+    price: 800,
+    assetImage: "assets/images/zenith_voyager.png",
+    movement: "Swiss Movement Battery Operated",
+    case: "44mm Platinum"
+  ),
+  Watch(
+    name: "Enchantment",
+    provider: "Trilobe",
+    price: 1700,
+    assetImage: "assets/images/trilobe_enchantment.png",
+    movement: "Japanese Movement Quartz",
+    case: "40mm Rose Gold Plated"
+  ),
+  Watch(
+    name: "Refinement",
+    provider: "Longines",
+    price: 1300,
+    assetImage: "assets/images/longines_refinement.png",
+    movement: "Swiss Movement Mechanical",
+    case: "36mm Polished Silver"
+  ),
+  Watch(
+    name: "Luminance",
+    provider: "Luminor",
+    price: 1900,
+    assetImage: "assets/images/luminor_luminance.png",
+    movement: "Swiss Movement Automatic",
+    case: "45mm Black PVD Coated"
+  ),
+  Watch(
+    name: "Navigator",
+    provider: "Zenith",
+    price: 1000,
+    assetImage: "assets/images/zenith_navigator.png",
+    movement: "Swiss Movement Battery Operated",
+    case: "42mm Yellow Gold Plated"
+  ),
+  Watch(
+    name: "Illumination",
+    provider: "Trilobe",
+    price: 1500,
+    assetImage: "assets/images/trilobe_illumination.png",
+    movement: "Japanese Movement Quartz",
+    case: "40mm Polished White Gold"
+  ),
 ];
